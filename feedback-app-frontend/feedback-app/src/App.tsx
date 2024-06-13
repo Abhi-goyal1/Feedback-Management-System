91edca1a99a8c484cde0dd3ea835438eadad016b
abhi goyal <abhigoyal72621289@gmail.com>
2024-06-13 10:28:18 +0530

    typescript

--- feedback-app-frontend/feedback-app/src/App.tsx
+++ feedback-app-frontend/feedback-app/src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Feedback Management System</h1>
+      <h5>Your Feedback Important For Us</h5>
       <FeedbackForm />
       <FeedbackList />
     </div>
